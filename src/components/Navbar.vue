13f3edb0e4d019489fb35abebb63f47b4483a983
Fahmi Affan <fahmiaffan07@gmail.com>
2023-02-24 05:42:54 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"
+          ><i class="fas fa-bars"></i
+        ></a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <button class="btn btn-sm btn-danger">
+          <i class="fas fa-sign-out-alt"></i>Logout
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style>
+</style>
